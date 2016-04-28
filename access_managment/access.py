6861a7bd763139f190b96e15e4cc8d237ffa42e2
ZHOU Peikun <zpkhhx@gmail.com>
2016-04-28 11:48:15 +0200

    print result to xls fils

--- access_managment/access.py
+++ access_managment/access.py
@@ -1,9 +1,11 @@
 import os
 import time
 
-event_file = open('C:/Users/pzhou/Desktop/events_0427.csv', 'r')
+event_file = open('C:/Users/pzhou/Desktop/access_managment/test.csv', 'r')
 
-today_time = time.strftime("%d/%m/%Y", time.localtime())
+# today_time = time.strftime("%d/%m/%Y", time.localtime())
+today_time = '27/04/2016'
+
 print today_time
 # time format, 06/01/2090  17:33:18
 # event format, 27/04/2016  14:53:08;0006;DING Jiang (6);Access allowed;Right Entrance (side 1)
@@ -11,13 +13,43 @@ print today_time
 today_recording = []
 
 for line in event_file:
-	if line[:10] == today_time and line.find('Access'):
-		today_recording.append(line)
+	if line[:10] == today_time: 
+		if line.split(";")[3] == ("Access allowed"):
+			today_recording.append(line)
 #		print line
-print today_recording[0]
+# print today_recording[0][21:25]
 
+#print today_recording[0] 
+
+#print today_recording
+print len(today_recording)
+
+name_last = {}
+name_first = {}
+l1=[]
+l2=[]
 for log in today_recording:
-	
+	if name_last.has_key(log.split(";")[2]):
+		name_last[log.split(";")[2]] = log.split(";")[0]
+	else:
+		name_last[log.split(";")[2]] = log.split(";")[0]
+		name_first[log.split(";")[2]] = log.split(";")[0]
+		
+print name_last
+print name_first
+print len(name_last)
+print len(name_first)
 
+result = {}
+for (k1, v1) in name_last.items():
+	for (k2, v2) in name_first.items():
+		if k1 == k2:
+			result[k1] = v2 + "\t" + v1[12:]
+		else:
+			continue
+print result
 
+result_file = open("result.xls", 'w')
 
+for key in result:
+	result_file.write(key + '\t' + result[key] + '\n')
