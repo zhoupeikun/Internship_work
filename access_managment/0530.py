73a79dd2cc5e6db56af75b0f6239a9b6182b78b2
ZHOU Peikun <zpkhhx@gmail.com>
2016-06-10 16:35:18 +0200

    add absence print

--- access_managment/0530.py
+++ access_managment/0530.py
@@ -1,3 +1,6 @@
+ #!/usr/local/bin/python
+ # coding: utf-8
+
 import os
 import time
 import xlwt
@@ -11,6 +14,7 @@ print today_time
 # time format, 06/01/2090  17:33:18
 # event format, 27/04/2016  14:53:08;0006;DING Jiang (6);Access allowed;Right Entrance (side 1)
 
+# today_recording store today's events or the select day's events
 today_recording = []
 
 for line in event_file:
@@ -19,12 +23,17 @@ for line in event_file:
 			today_recording.append(line)
 #		print line
 # print today_recording[0][21:25]
-
-#print today_recording[0] 
+# print today_recording[0] 
 
 #print today_recording
-print len(today_recording)
+#print len(today_recording)
 
+name_list = ['ZHUANG Jie(1)', 'WANG Xiaoyou(2)', 'CAO Peng(3)', 'VON KUNOW Moritz(4)', 'XU Chi (5)', 'DING Jiang (6)', 'WANG Yue', 'JIANG Jie', 'CHEN Li (9)', 'WANG Xuemei (10)', 'MANSO Guillaume (11)', 'RAMELLI Jerome', 'Lambot Jean-Marc (14)', 
+			'ZHOU Bin (15)', 'OLDING Jennifer-secondcard', 'OLDING Jennifier (19)', 'PADILLA MAYER Helios (16)', 'HEUSEL Michael (17)', 'BURGUN Jean-Yves(18) ', 'ZONG Yuanyuan', 'Intern Receptionist', 'Xu Fei (20)', 'LI Yidai (21)', 'LIU-GERHARDS Yang (22)', 'HENG Ze', 'ZHOU Peikun ', 'RISK Intern']
+print len(name_list)
+
+# name_first store the first event of each one
+# name_last  store the last event of each one
 name_last = {}
 name_first = {}
 l1=[]
@@ -41,14 +50,30 @@ print name_first
 print len(name_last)
 print len(name_first)
 
+
 result = {}
-for (k1, v1) in name_last.items():
-	for (k2, v2) in name_first.items():
-		if k1 == k2:
-			result[k1] = v2 + "\t" + v1[12:]
+for name in name_list:
+	for (k1, v1) in name_last.items():
+		for (k2, v2) in name_first.items():
+			if name == k1 and k1 == k2:
+				result[k1] = v2[:12] + "\t" + v2[12:] + '\t' + v1[12:]
+				#name_list.remove(name)
+			else:
+				continue
+	#result[]
+
+print result
+print len(result)
+
+for key in result:
+	for name in name_list:
+		if key == name:
+			name_list.remove(name)
 		else:
 			continue
-print result
+print name_list
+	
+
 
 result_file = open("result.xls", 'w')
 workbook = xlwt.Workbook(encoding = 'ascii')
@@ -56,12 +81,16 @@ worksheet = workbook.add_sheet('sheet')
 
 
 for key in result:
-	if int(result[key][12:14]) > 8 :
-		if int(result[key][21:23]) < 17 :
-			result_file.write(key + '\t' + result[key] + '\tArrived Later and Left Later' + '\n')
+	if int(result[key][13:15]) > 8 :
+		if int(result[key][22:24]) < 17 :
+			result_file.write(key + '\t' + result[key] + '\t Arrived later and left early' + '\n')
+			#worksheet.write()
 		else :
-			result_file.write(key + '\t' + result[key] + '\tArrived Later' + '\n')
-	elif int(result[key][21:23]) < 17 :
-		result_file.write(key + '\t' + result[key] + '\tLeft early' + '\n')
+			result_file.write(key + '\t' + result[key] + '\t Arrived later' + '\n')
+	elif int(result[key][22:24]) < 17 :
+		result_file.write(key + '\t' + result[key] + '\t Left early' + '\n')
 	else: 
 		result_file.write(key + '\t' + result[key] + '\n')
+
+for name in name_list:
+	result_file.write(name + '\t Absence' + '\n')
